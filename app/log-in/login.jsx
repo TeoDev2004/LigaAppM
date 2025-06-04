a6279bd30d249a73953268aa060839c874730936
TeoDev2004 <mateobdevelop@gmail.com>
2025-06-04 11:06:17 -0500

    ultima añadidura

--- app/log-in/login.jsx
+++ app/log-in/login.jsx
@@ -1,36 +1,22 @@
-/*import { useRouter } from "expo-router";*/
 import { useAudioPlayer } from "expo-audio";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, StyleSheet, View } from "react-native";
 
 const audioSource = require("../../assets/sounds/PokemonIndex.mp3");
 
 export default function Login() {
-  /*const router = useRouter();
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>LOGIN SCREEN</Text>
-      <Button title="Volver" onPress={() => router.back()}></Button>
-    </View>
-  );*/
   const player = useAudioPlayer(audioSource);
 
-  // player.ready es true cuando el audio está cargado y listo para usar
-  const canPlay = player.ready;
+  // Este useEffect se ejecutará cuando player.ready cambie a true
+  useEffect(() => {
+    if (player.ready) {
+      player.play();
+    }
+  }, [player.ready]);
 
   return (
     <View style={styles.container}>
-      <Button
-        title="Play Sound"
-        onPress={() => player.play()}
-        //disabled={!canPlay}
-      />
+      <Button title="Play Sound" onPress={() => player.play()} />
     </View>
   );
 }
